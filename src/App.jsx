a75b14ae528ef20662d643c7a5e2e4746656224d
Raymond <raymondurrutia2@yahoo.com>
2025-08-22 06:20:56 -0400

    Create start exploring component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./style.css";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
+import StartExploring from "./components/StartExploring/StartExploring";
 
 export default function App() {
   return (
     <>
       <Header />
       <SearchBar />
+      <StartExploring />
     </>
   );
 }
